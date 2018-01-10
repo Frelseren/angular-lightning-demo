455cc0a3f09f3a83f3fd22ae4b94e7f98e136856
Nikita Verkhoshintcev <verkhoshintsev@gmail.com>
2018-01-10 23:42:24 +0200

    contact subsribe done

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,4 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, ParamMap } from '@angular/router';
+import { Observable } from 'rxjs/Observable';
+
+interface Contact {
+  Id: string;
+  Name: string;
+  Title: string;
+  Phone: string;
+  Email: string;
+}
+
+declare const Visualforce;
 
 @Component({
   selector: 'app-contact',
@@ -6,10 +18,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact.component.scss']
 })
 export class ContactComponent implements OnInit {
+  route$: Observable<ParamMap>;
+  contact: Contact;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute
+  ) {
+    this.route$ = route.paramMap;
+  }
 
   ngOnInit() {
+    this.route$.subscribe(map => {
+      Visualforce.remoting.Manager.invokeAction(
+        'AngularPOC.getContact',
+        map.get('contactId'),
+        (result: Contact, event) => {
+          if (event.status) {
+            this.contact = result;
+          } else {
+            console.error(event.message);
+          }
+        },
+        { escape: false }
+      );
+    });
   }
 
 }
--- src/app/contacts/contacts.component.ts
+++ src/app/contacts/contacts.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
 
+declare const Visualforce;
+
+interface Contact {
+  Id: string;
+  Name: string;
+}
+
 @Component({
   selector: 'app-contacts',
   templateUrl: './contacts.component.html',
   styleUrls: ['./contacts.component.scss']
 })
 export class ContactsComponent implements OnInit {
-
-  constructor() { }
+  contacts: Contact[];
 
   ngOnInit() {
+    Visualforce.remoting.Manager.invokeAction(
+      'AngularPOC.getContacts',
+      (result: Contact[], event) => {
+        if (event.status) {
+          this.contacts = result;
+        } else {
+          console.error(event.message);
+        }
+      },
+      { escape: false }
+    );
   }
 
 }
